1e3a5497307d634ea806a2da5f9b2746170b664b
rynraguro <raguroruelyn06@gmail.com>
2025-03-28 10:05:17 +0800

    Added configuration for Guzzle services and created User1Service and User2Service

--- config/services.php
+++ config/services.php
@@ -1,4 +1,10 @@
 <?php
+
 return [
-    // Service configurations will be added later
+    'users1' => [
+        'base_uri' => env('USERS1_SERVICE_BASE_URL'),
+    ],
+    'users2' => [
+        'base_uri' => env('USERS2_SERVICE_BASE_URL'),
+    ],
 ];
